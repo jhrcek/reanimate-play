0bbc33a1557bec974eb97bdb42bde6d8067e3af8
Jan Hrček <honza.hrk@gmail.com>
2020-09-24 19:23:51 +0200

    Mess

--- src/Main.hs
+++ src/Main.hs
@@ -8,152 +8,56 @@
 
 module Main (main) where
 
-import Control.Lens ((&), (.~))
-import qualified Data.Text as Text
-import qualified Data.Text.IO as Text
-import Graphics.SvgTree --(LineJoin (JoinBevel), strokeLineJoin, pattern None)
+--(LineJoin (JoinBevel), strokeLineJoin, pattern None)
+
+import Control.Monad (forM_)
+import Data.List
+import Graphics.SvgTree
 import Reanimate
-import Reanimate.Scene
-import Reanimate.Voice (fakeTranscript)
+import System.Random
+
+triples :: Ord a => [a] -> [(a, a, a)]
+triples (x : y : z : rest) = (x, y, z) : triples rest
 
 main :: IO ()
 main = do
-  ts <- fakeTranscript <$> Text.readFile "/home/jhrcek/Devel/github.com/jhrcek/reanimate-play/transcript.txt"
+  randomTriples <- take 10 . triples . randomRs (0, 2 * pi) <$> newStdGen
+
   reanimate $
     addStatic (mkBackground "black") $
       scene $ do
-        titleScene
+        alpha <- newVar 0
+        beta <- newVar 0
+        gamma <- newVar 0
+        newSprite_ $ view <$> unVar alpha <*> unVar beta <*> unVar gamma
 
-        --fork $ annotateWithTranscript ts
+        forM_ randomTriples $ \(a, b, c) -> do
+          tweenVar alpha 1 $ \val -> fromToS val a
+          fork $ tweenVar beta 1 $ \val -> fromToS val b
+          fork $ tweenVar gamma 1 $ \val -> fromToS val c
+          wait 0.5
 
-        circleDrawProgress <- newVar 0
-        diameterDrawProgress <- newVar 0
-        triangleDrawProgress <- newVar 0
-        radiusDrawProgress <- newVar 0
-        centerDrawProgress <- newVar 0
-        endpointsDrawProgress <- newVar 0
-        cTheta <- newVar $ 2 * pi / 3
-
-        newSprite_ $
-          fmap thales $
-            SceneVars
-              <$> unVar circleDrawProgress
-              <*> unVar diameterDrawProgress
-              <*> unVar triangleDrawProgress
-              <*> unVar radiusDrawProgress
-              <*> unVar centerDrawProgress
-              <*> unVar endpointsDrawProgress
-              <*> unVar cTheta
-
-        -- waitUntil $ wordStart $ findWord ts ["circle"] "circle"
-        tweenVar circleDrawProgress 2 $ \val -> fromToS val 1
-        wait 1
-        --waitUntil $ wordStart $ findWord ts ["circle"] "center"
-        -- TODO some signal to dedupe the "highlight"
-        tweenVar centerDrawProgress 0.5 $ \val -> fromToS val 3
-        tweenVar centerDrawProgress 0.5 $ \val -> fromToS val 1
-        wait 1
-        --waitUntil $ wordStart $ findWord ts ["diameter"] "diameter"
-        tweenVar diameterDrawProgress 1 $ \val -> fromToS val 1
-        wait 1
-        --waitUntil $ wordStart $ findWord ts ["diameter"] "endpoints"
-        tweenVar endpointsDrawProgress 0.5 $ \val -> fromToS val 3
-        tweenVar endpointsDrawProgress 0.5 $ \val -> fromToS val 1
-        wait 1
-        --waitUntil $ wordStart $ findWord ts ["point"] "point"
-        tweenVar triangleDrawProgress 1 $ \val -> fromToS val 1
-
-        wait 1
-        tweenVar cTheta 1 $ \val -> fromToS val (pi / 2) . curveS 2
-        wait 1
-        tweenVar cTheta 1 $ \val -> fromToS val (pi / 3) . curveS 2
-        wait 1
-        tweenVar cTheta 1 $ \val -> fromToS val (pi / 10) . curveS 2
-        wait 1
-        tweenVar cTheta 1 $ \val -> fromToS val (pi * 2 / 3) . curveS 2
-        wait 1
-        tweenVar radiusDrawProgress 1 $ \val -> fromToS val 1
-        wait 5
-
-titleScene :: Scene s ()
-titleScene = do
-  title <-
-    oNew $
-      withStrokeColor "white" $
-        withFillColor "whiteu" $
-          withStrokeWidth 0.1 $
-            scale 1.5 $
-              center $ latex "Thales's theorem"
-  oShowWith title oDraw
-  wait 1
-  oHideWith title oFadeOut
-  wait 1
-
-thales :: SceneVars -> SVG
-thales SceneVars {..} =
-  let cx = 4 * cos cTheta
-      cy = 4 * sin cTheta
+view :: Double -> Double -> Double -> SVG
+view alpha beta gamma =
+  let r = 4.0
    in withStrokeColor "white" $
         withFillOpacity 0 $
           withStrokeWidth (defaultStrokeWidth * 0.4) $
             mkGroup
-              [ mkClipPath "triangle-mask" $ removeGroups $ mkLinePath [(-4, 0), (cx, cy), (4, 0)],
-                drawIf (triangleDrawProgress > 0.99) $
-                  withFillOpacity 1 $
-                    mkGroup
-                      [ translate (1.05 * cx) (1.05 * cy) $ textLabel "C",
-                        withFillOpacity 0 $ mkCircle 0.05
-                      ],
-                partial circleDrawProgress $ mkCircle 4,
-                partial diameterDrawProgress $ mkLine (-4, 0) (4, 0),
-                -- Triangle
-                partial triangleDrawProgress $
-                  mkLinePath [(-4, 0), (cx, cy), (4, 0)]
-                    & strokeLineJoin .~ pure JoinBevel,
-                partial radiusDrawProgress $ mkLine (0, 0) (cx, cy),
-                -- angles
-                drawIf (triangleDrawProgress > 0.99) $
-                  withClipPathRef (Ref "triangle-mask") $
-                    mkGroup
-                      [ translate cx cy $ mkCircle 0.5,
-                        translate 4 0 $ mkCircle 0.5,
-                        translate -4 0 $ mkCircle 0.5
-                      ],
-                -- Endpoints
-                withFillOpacity 1 $
-                  mkGroup
-                    [ translate -4.0 0 $ mkCircle (endpointsDrawProgress * 0.05),
-                      translate 4.0 0 $ mkCircle (endpointsDrawProgress * 0.05),
-                      drawIf (endpointsDrawProgress > 0.99) $
-                        mkGroup
-                          [ translate -4.2 0 $ textLabel "A",
-                            translate 4.2 0 $ textLabel "B"
-                          ]
-                    ],
-                -- Center
-                withFillOpacity 1 $
-                  mkGroup
-                    [ mkCircle (centerDrawProgress * 0.05),
-                      drawIf (centerDrawProgress > 0.99) $
-                        translate 0 -0.4 $ textLabel "O"
-                    ]
+              [ mkCircle r,
+                mkLinePath
+                  [ anglePoint r alpha,
+                    anglePoint r beta,
+                    anglePoint r gamma,
+                    anglePoint r alpha
+                  ]
               ]
 
-partial :: Double -> SVG -> Tree
-partial progress = partialSvg progress . pathify
-
-textLabel :: Text.Text -> Tree
-textLabel = withFillColor "white" . scale 0.2 . mkText
+anglePoint :: Double -> Double -> (Double, Double)
+anglePoint radius angle =
+  ( radius * cos angle,
+    radius * sin angle
+  )
 
 drawIf :: Bool -> SVG -> SVG
 drawIf cond svg = if cond then svg else None
-
-data SceneVars = SceneVars
-  { circleDrawProgress :: Double,
-    diameterDrawProgress :: Double,
-    triangleDrawProgress :: Double,
-    radiusDrawProgress :: Double,
-    centerDrawProgress :: Double,
-    endpointsDrawProgress :: Double,
-    cTheta :: Double
-  }
---
+++ src/ThalesTry2.hs
@@ -0,0 +1,156 @@
+#!/usr/bin/env stack
+-- stack runghc --package reanimate
+
+{-# LANGUAGE NegativeLiterals  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# OPTIONS_GHC -Wall #-}
+
+module Main (main) where
+
+import Control.Lens ((&), (.~))
+import qualified Data.Text as Text
+--(LineJoin (JoinBevel), strokeLineJoin, pattern None)
+
+import Graphics.SvgTree
+import Reanimate
+import Reanimate.Scene
+import Reanimate.Voice (TWord (..), Transcript (..), annotateWithTranscript, findWord,
+                        loadTranscript, wordStart)
+
+ts0 :: Transcript
+ts0 = loadTranscript "/home/jhrcek/Devel/github.com/jhrcek/reanimate-play/transcript.txt"
+
+main :: IO ()
+main = do
+  reanimate $
+    addStatic (mkBackground "black") $
+      scene $ do
+        titleDuration <- withSceneDuration titleScene
+        let ts = postponeTranscript titleDuration ts0
+        fork $ annotateWithTranscript ts
+
+        aP <- newVar 0
+        bP <- newVar 0
+        cP <- newVar 0
+        circleP <- newVar 0
+        diameterP <- newVar 0
+        triangleP <- newVar 0
+        cTheta <- newVar $ 2 * pi / 3
+
+        newSprite_ $
+          fmap thales $
+            SceneVars
+              <$> unVar aP
+              <*> unVar bP
+              <*> unVar cP
+              <*> unVar circleP
+              <*> unVar diameterP
+              <*> unVar triangleP
+              <*> unVar cTheta
+        
+        waitUntil $ wordStart $ findWord ts ["statement"] "geometry"
+        tweenVar circleP 2 $ \val -> fromToS val 1
+
+        waitUntil $ wordStart $ findWord ts ["statement"] "A"
+        fork $ do 
+            tweenVar aP 0.5 $ \val -> fromToS val 2
+            tweenVar aP 0.5 $ \val -> fromToS val 1
+        waitUntil $ wordStart $ findWord ts ["statement"] "B"
+        fork $ do 
+            tweenVar bP 0.5 $ \val -> fromToS val 2
+            tweenVar bP 0.5 $ \val -> fromToS val 1
+        waitUntil $ wordStart $ findWord ts ["statement"] "C"
+        fork $ do 
+            tweenVar cP 0.5 $ \val -> fromToS val 2
+            tweenVar cP 0.5 $ \val -> fromToS val 1
+
+        waitUntil $ wordStart $ findWord ts ["statement"] "AC"
+        tweenVar diameterP 1 $ \val -> fromToS val 1
+
+        waitUntil $ wordStart $ findWord ts ["statement"] "then"
+        tweenVar triangleP 0.5 $ \val -> fromToS val 1
+
+titleScene :: Scene s ()
+titleScene = do
+  title <-
+    oNew $
+      withStrokeColor "white" $
+        withFillColor "whiteu" $
+          withStrokeWidth 0.1 $
+            scale 1.5 $
+              center $ latex "Thales's theorem"
+  oShowWith title oDraw
+  wait 1
+  oHideWith title oFadeOut
+  wait 1
+
+thales :: SceneVars -> SVG
+thales SceneVars {..} =
+  let cx = 4 * cos cTheta
+      cy = 4 * sin cTheta
+      r = 4.0
+   in withStrokeColor "white" $
+        withFillOpacity 0 $
+          withStrokeWidth (defaultStrokeWidth * 0.4) $
+            mkGroup
+              [ partial circleP $ mkCircle 4,
+                partial diameterP $ mkLine (-4, 0) (4, 0),
+                mkGroup
+                  [ translate (- r) 0 $ mkCircle (aP * 0.05),
+                    withFillOpacity aP $ translate (-1.05 * r) 0 $ textLabel "A"
+                  ],
+                mkGroup
+                  [ translate cx cy $ mkCircle (bP * 0.05),
+                    withFillOpacity bP $ translate (1.05 * cx) (1.05 * cy) $ textLabel "B"
+                  ],
+                mkGroup
+                  [ translate r 0 $ mkCircle (cP * 0.05),
+                    withFillOpacity cP $ translate (1.05 * r) 0 $ textLabel "C"
+                  ],
+                -- Center
+                withFillOpacity 1 $
+                  mkGroup
+                    [ mkCircle (diameterP * 0.05),
+                      drawIf (diameterP > 0.99) $
+                        translate 0 -0.4 $ textLabel "O"
+                    ],
+                partial triangleP $
+                  mkLinePath [(-4, 0), (cx, cy), (4, 0)]
+                    & strokeLineJoin .~ pure JoinBevel
+              ]
+
+partial :: Double -> SVG -> Tree
+partial progress = partialSvg progress . pathify
+
+textLabel :: Text.Text -> Tree
+textLabel = withFillColor "white" . withStrokeWidth 0 . scale 0.2 . mkText
+
+drawIf :: Bool -> SVG -> SVG
+drawIf cond svg = if cond then svg else None
+
+-- P stands for drawing Progresss (number betwee 0 - 1)
+data SceneVars = SceneVars
+  { aP        :: Double,
+    bP        :: Double,
+    cP        :: Double,
+    circleP   :: Double,
+    diameterP :: Double,
+    triangleP :: Double,
+    cTheta    :: Double
+  }
+
+postponeTranscript :: Double -> Transcript -> Transcript
+postponeTranscript delta Transcript {..} =
+  Transcript
+    { transcriptWords = fmap postponeWord transcriptWords,
+      ..
+    }
+  where
+    postponeWord :: TWord -> TWord
+    postponeWord TWord {..} =
+      TWord
+        { wordStart = wordStart + delta,
+          wordEnd = wordEnd + delta,
+          ..
+        }
